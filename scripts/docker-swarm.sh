908938d265941518c5d0cc35f7301ac04ab1c459
MarioCakeDev <weberlein.david+github@gmail.com>
2023-05-13 11:23:10 +0200

    Create docker-swarm.sh

---
+++ scripts/docker-swarm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker network create \
+--driver overlay \
+  portainer_agent_network
+  
+docker service create \
+  --name portainer_agent \
+  --network portainer_agent_network \
+  --publish mode=host,target=9001,published=9001,protocol=tcp \
+  --mode global \
+  --constraint 'node.platform.os == linux' \
+  --mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
+  --mount type=bind,src=//var/lib/docker/volumes,dst=/var/lib/docker/volumes \
+  portainer/agent:latest
